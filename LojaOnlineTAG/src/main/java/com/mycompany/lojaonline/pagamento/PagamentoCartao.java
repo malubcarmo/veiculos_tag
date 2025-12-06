50f2ebf8efdb01841bc32d3efa8e97782d1a1d3e
Maria Carmo <94873518+malubcarmo@users.noreply.github.com>
2025-12-06 19:39:25 -0300

    Refactor payment processing in PagamentoCartao

--- LojaOnlineTAG/src/main/java/com/mycompany/lojaonline/pagamento/PagamentoCartao.java
+++ LojaOnlineTAG/src/main/java/com/mycompany/lojaonline/pagamento/PagamentoCartao.java
@@ -1,16 +1,22 @@
 package com.mycompany.lojaonline.pagamento;
 
+import java.util.UUID; // Importação necessária
+
 public class PagamentoCartao extends Pagamento {
 
     @Override
     public void processarPagamento(double valorTotal) {
         System.out.println("\n--- PROCESSANDO CARTÃO DE CRÉDITO ---");
         System.out.println("Conectando com a operadora...");
-
-        // Simulação de parcelamento
-        System.out.println("Opções de parcelamento:");
+        
+        System.out.println("Opções de pagamento:");
         System.out.printf("1x de R$ %.2f (sem juros)%n", valorTotal);
-
+        
         System.out.println(">> Transação Aprovada!");
-    }
+        
+        // --- GERADOR DE CÓDIGO DE RASTREIO ---
+        String codigoRastreio = UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+        System.out.println(">> CÓDIGO DE RASTREIO: TRK-" + codigoRastreio);
+        System.out.println("Guarde este código para acompanhar seu pedido.");
+    }
 }

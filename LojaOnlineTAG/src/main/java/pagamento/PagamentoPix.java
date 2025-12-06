fc9122970864970a2d6ba40c9799ffb13c499083
heiki1 <heiki1chan@gmail.com>
2025-12-06 20:24:15 -0300

    remove comments

--- LojaOnlineTAG/src/main/java/pagamento/PagamentoPix.java
+++ LojaOnlineTAG/src/main/java/pagamento/PagamentoPix.java
@@ -6,7 +6,7 @@ public class PagamentoPix extends Pagamento {
 
     @Override
     public void processarPagamento(double valorTotal) {
-        // Regra: Pix tem 10% de desconto
+        // Regra de Pix tem 10% de desconto
         double valorComDesconto = valorTotal * 0.90;
         
         System.out.println("\n--- PROCESSANDO PIX ---");
@@ -16,8 +16,7 @@ public class PagamentoPix extends Pagamento {
         System.out.printf("Valor com 10%% de desconto: R$ %.2f%n", valorComDesconto);
         
         System.out.println(">> Pagamento Confirmado!");
-        
-        // --- GERADOR DE CÓDIGO DE RASTREIO ---
+
         // Gera um código aleatório, pega os primeiros 8 caracteres e deixa maiúsculo
         String codigoRastreio = UUID.randomUUID().toString().substring(0, 8).toUpperCase();
         System.out.println(">> CÓDIGO DE RASTREIO: TRK-" + codigoRastreio);
--- LojaOnlineTAG/src/main/java/service/Carrinho.java
+++ LojaOnlineTAG/src/main/java/service/Carrinho.java
@@ -50,5 +50,3 @@ public class Carrinho {
         itens.clear();
     }
 }
-
-// apaga malu
